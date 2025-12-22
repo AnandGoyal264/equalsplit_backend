664c5625592140189afed4c29d02202c207d7ef3
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-12-22 21:44:27 +0530

    test_added

--- src/app.js
+++ src/app.js
@@ -8,12 +8,18 @@ import balanceRoutes from "./routes/balance.routes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://equisplit-puce.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/groups", groupRoutes);
 app.use("/api/expenses", expenseRoutes);
 app.use("/api/balances", balanceRoutes);
+app.get('/test',(req,res)=>{
+  res.send({'welcome':'user'})
+});
 
 export default app;
