11b63e7d69d805a49dce23e06dd2908eb4244672
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-12-24 01:50:24 +0530

    last_time_change

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -3,7 +3,8 @@ import { auth } from "../middlewares/auth.middleware.js";
 import { addExpense ,getGroupExpenses} from "../controllers/expense.controller.js";
 
 const router = Router();
-router.get("/groups/:groupId", auth ,getGroupExpenses );
 router.post("/", auth, addExpense);
+router.get("/groups/:groupId", auth ,getGroupExpenses);
+
 
 export default router;
