a7263491c9a389841d86e72f9b2e3bf9ffb3962e
AnandGoyal264 <anandgoyal9171@gmail.com>
2025-12-24 01:24:08 +0530

    hgg

--- src/routes/expense.routes.js
+++ src/routes/expense.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { auth } from "../middlewares/auth.middleware.js";
-import { addExpense } from "../controllers/expense.controller.js";
+import { addExpense ,getGroupExpenses} from "../controllers/expense.controller.js";
 
 const router = Router();
-
+router.get("/groups/:groupId", auth ,getGroupExpenses );
 router.post("/", auth, addExpense);
 
 export default router;
